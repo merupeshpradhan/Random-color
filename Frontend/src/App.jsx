a9fef0bdf6162f5a4327c3b8478b5fca4fdd9c72
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-07-14 17:07:52 +0530

    Project fully complete

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import RandomColor from "./random-color";
+
+function App() {
+  return (
+    <div className="app">
+    {/* Random color component */}
+      <RandomColor />
+    </div>
+  );
+}
+
+export default App;
